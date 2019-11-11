85a966b883cbb5e12d45496600139e3c37dca148
Pieter Wuille <pieter.wuille@gmail.com>
2019-11-11 14:35:23 -0800

    Make COMB_BLOCKS and COMB_TEETH configurable, and test in Cirrus

--- ci/cirrus.sh
+++ ci/cirrus.sh
@@ -19,7 +19,7 @@ sed -i'' -e 's@/usr/bin/file@$(which file)@g' configure
 ./configure \
     --enable-experimental="$EXPERIMENTAL" \
     --with-test-override-wide-multiply="$WIDEMUL" --with-asm="$ASM" \
-    --enable-ecmult-static-precomputation="$STATICPRECOMPUTATION" --with-ecmult-gen-precision="$ECMULTGENPRECISION" \
+    --enable-ecmult-static-precomputation="$STATICPRECOMPUTATION" --with-ecmult-gen-blocks="$ECMULTGENBLOCKS" --with-ecmult-gen-teeth="$ECMULTGENTEETH" \
     --enable-module-ecdh="$ECDH" --enable-module-recovery="$RECOVERY" \
     --enable-module-schnorrsig="$SCHNORRSIG" \
     --with-valgrind="$WITH_VALGRIND" \
